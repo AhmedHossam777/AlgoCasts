173df7b49a7588f3419b0134214bb3e8c0d364f6
Ahmed Hossam <ahmedhossam9226@gmail.com>
2024-03-30 00:40:27 +0200

    reverse string problem

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -2,10 +2,52 @@
 // Given a string, return a new string with the reversed
 // order of characters
 // --- Examples
-//   reverse('apple') === 'leppa'
+//   reverse('apple') === 'elppa'
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  return str.split('').reduce((acc, char) => {
+    return char + acc;
+  }, '');
+}
+console.log(reverse('apple'));
 
 module.exports = reverse;
+
+//? first solution
+// function reverse(str) {
+// 	let arr = str.split('')
+// 	let min = 0
+// 	let max = arr.length -1
+
+// 	while (min < max) {
+// 		let temp = arr[min];
+// 		arr[min] = arr[max];
+// 		arr[max] = temp;
+// 		min ++;
+// 		max --;
+// 	}
+// 	return arr.join('')
+
+// }
+
+// console.log(reverse('apple')); // elppa
+
+//? second solution
+// function reverse(str) {
+// 	return str.split('').reverse().join('');
+// }
+
+// console.log(reverse('apple')); // elppa
+
+//? Third solution
+// function reverse(str) {
+// 	let reversed = '';
+// 	for (const char of str) {
+// 		reversed = char + reversed;
+// 	}
+// 	return reversed;
+// }
+
+// console.log(reverse('apple')); // elppa
