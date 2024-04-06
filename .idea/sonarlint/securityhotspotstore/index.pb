cef3e87445b781a22beb57cab21194194992c60d
Ahmed Hossam <ahmedhossam9226@gmail.com>
2024-04-06 08:51:03 +0200

    solve maxChar problem

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,5 @@
+
+9
+	README.md,8/e/8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+J
+exercises/maxchar/index.js,7/9/79593197641859d06cfc599c9c8649a11f5a4471
--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,26 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+const maxChar = (str) => {
+	const chars = {};
+	let max = 0;
+	let maxChar = '';
+	for (let char of str) {
+		if (!chars[char]) {
+			chars[char] = 1;
+		} else {
+			chars[char]++;
+		}
+	}
+
+	for (let charsKey in chars) {
+		if (max < chars[charsKey]) {
+			max = chars[charsKey];
+			maxChar = charsKey;
+		}
+	}
+	return maxChar;
+
+};
 
 module.exports = maxChar;
