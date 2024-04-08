c3bf84e54fce781e60142d718d546dd36d319fcf
Ahmed Hossam <ahmedhossam9226@gmail.com>
2024-04-08 06:47:59 +0200

    solve fizzbuzz problem

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -12,6 +12,19 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+function fizzBuzz(n) {
+  for (let i = 1; i <= n; i++) {
+    if (i % 5 === 0 && i % 3 === 0) {
+      console.log('fizzbuzz');
+    } else if (i % 5 === 0) {
+      console.log('buzz');
+    } else if (i % 3 === 0) {
+      console.log('fizz');
+    } else {
+      console.log(i);
+    }
+  }
+}
+fizzBuzz(15);
 
 module.exports = fizzBuzz;
