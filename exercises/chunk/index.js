422a9a1e1efbdc6f877e84ae8b0619c175712e13
Ahmed Hossam <ahmedhossam9226@gmail.com>
2024-07-11 17:45:02 +0300

    solve chunk problem

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -38,19 +38,43 @@
 //   return chunked;
 // }
 
+// const chunks = (arr, n) => {
+//   let numberOfChunks = arr.length / n;
+//   const chunked = [];
+//   let end = n;
+//   let start = 0;
+//   for (let i = 0; i < numberOfChunks; i++) {
+//     chunked.push(arr.slice(start, end));
+//     start = end;
+//     end += n;
+//   }
+//   return chunked;
+// };
 
+// console.log(chunks([1, 2, 3, 4, 5, 6, 7, 8], 3));
+//
 
-const chunks = (arr, n)=>{
-  let numberOfChunks = arr.length / n
-  const chunked = [];
+const chunks = (arr, n) => {
+  let numberOfChunks = (arr.length - 1) / n;
+  let chunked = [];
+  let newArr = [];
   let end = n;
-  let start = 0
-  for(let i = 0; i < numberOfChunks; i ++){
-    chunked.push(arr.slice(start, end))
-    start = end ;
+  let start = 0;
+
+  for (let j = start; j <= numberOfChunks; j++) {
+    for (let i = start; i < end; i++) {
+      if (arr[i] === undefined) {
+        continue;
+      }
+      newArr.push(arr[i]);
+    }
+    chunked.push(newArr);
+    newArr = [];
+    start = end;
     end += n;
   }
-  return chunked
-}
 
-console.log(chunks([1, 2, 3, 4, 5, 6, 7, 8], 3));
+  console.log(chunked);
+};
+
+chunks([1, 2, 3, 4, 5, 6, 7, 8], 3);
