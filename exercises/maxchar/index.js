a25e8ff9793461811a4180bdd55944dbc978c59d
Ahmed Hossam <ahmedhossam9226@gmail.com>
2024-07-13 09:51:15 +0300

    solve maxChar problem

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,26 +5,52 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
+// const maxChar = (str) => {
+// 	const chars = {};
+// 	let max = 0;
+// 	let maxChar = '';
+// 	for (let char of str) {
+// 		if (!chars[char]) {
+// 			chars[char] = 1;
+// 		} else {
+// 			chars[char]++;
+// 		}
+// 	}
+
+// 	for (let charsKey in chars) {
+// 		if (max < chars[charsKey]) {
+// 			max = chars[charsKey];
+// 			maxChar = charsKey;
+// 		}
+// 	}
+// 	return maxChar;
+
+// };
+
 const maxChar = (str) => {
-	const chars = {};
-	let max = 0;
-	let maxChar = '';
-	for (let char of str) {
-		if (!chars[char]) {
-			chars[char] = 1;
-		} else {
-			chars[char]++;
-		}
-	}
+  const strArr = str.split('');
+  const charMap = {};
+  let max = '';
+  let maxChar = '';
 
-	for (let charsKey in chars) {
-		if (max < chars[charsKey]) {
-			max = chars[charsKey];
-			maxChar = charsKey;
-		}
-	}
-	return maxChar;
+  for (let arrElement of strArr) {
+    if (!charMap[arrElement]) {
+      charMap[arrElement] = 1;
+    } else {
+      charMap[arrElement] += 1;
+    }
+  }
 
+  for (const char in charMap) {
+    if (max < charMap[char]) {
+      max = charMap[char];
+      maxChar = char;
+    }
+  }
+
+  return maxChar;
 };
+
+console.log(maxChar('abcccccccd'));
 
 module.exports = maxChar;
