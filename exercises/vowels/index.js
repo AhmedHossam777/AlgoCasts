e15f2da7ee35da23569bf689d04b0d908f87080e
Ahmed Hossam <ahmedhossam9226@gmail.com>
2024-07-22 14:48:58 +0300

    Solve vowels problem

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,22 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+	const vowels = ['a', 'e', 'i', 'o', 'u'];
+	const strArr = str.toLowerCase().split('')
+	let numberOfVowels = 0
+
+	for (let char of strArr) {
+		for (let i=0; i < vowels.length; i++) {
+			if (char === vowels[i]) {
+				numberOfVowels += 1;
+			}
+		}
+	}
+
+	return numberOfVowels;
+}
+
+console.log(vowels('Why do you ask?'));
 
 module.exports = vowels;
